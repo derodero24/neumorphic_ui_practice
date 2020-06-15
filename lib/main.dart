560659e1cd30be1724a10762f9a2963e94195253
derodero24 <k.itsh20524abc@gmail.com>
2020-06-15 16:08:15 +0900

    Add two buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // DEBUGバナー削除
-      title: 'title',
-      // theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: null,
-        body: Center(child: MyHomePage()),
-        bottomNavigationBar: null,
-        floatingActionButton: null,
+    return NeumorphicApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.light,
+      theme: const NeumorphicThemeData(
+        baseColor: Color(0xFFFFFFFF),
+        lightSource: LightSource.topLeft,
+        depth: 10,
       ),
+      darkTheme: const NeumorphicThemeData(
+        baseColor: Color(0xFF3E3E3E),
+        lightSource: LightSource.topLeft,
+        depth: 6,
+      ),
+      home: MyHomePage(),
     );
   }
 }
@@ -22,6 +28,64 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Text('stars');
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            NeumorphicButton(
+              onPressed: () {
+                print('onClick');
+              },
+              style: const NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape: NeumorphicBoxShape.circle(),
+              ),
+              padding: const EdgeInsets.all(12),
+              child: Icon(
+                Icons.favorite_border,
+                color: _iconColor(context),
+              ),
+            ),
+            NeumorphicButton(
+              margin: const EdgeInsets.only(top: 12),
+              onPressed: () {
+                NeumorphicTheme.of(context).themeMode =
+                    NeumorphicTheme.isUsingDark(context)
+                        ? ThemeMode.light
+                        : ThemeMode.dark;
+              },
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape:
+                    NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
+              ),
+              padding: const EdgeInsets.all(12),
+              child: Text('Toggle Theme',
+                  style: TextStyle(color: _textColor(context))),
+            ),
+//            NeumorphicButton(
+//              margin: const EdgeInsets.only(top: 12),
+//            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Color _iconColor(BuildContext context) {
+    if (NeumorphicTheme.isUsingDark(context)) {
+      return Colors.white;
+    } else {
+      return Colors.black;
+    }
+  }
+
+  Color _textColor(BuildContext context) {
+    if (NeumorphicTheme.isUsingDark(context)) {
+      return Colors.white;
+    } else {
+      return Colors.black;
+    }
   }
 }
